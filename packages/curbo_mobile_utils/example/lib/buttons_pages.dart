764faa232c7121f326d26692532211dd055e5bb7
Silvio Arzeno <silviodanielarzeno@gmail.com>
2025-05-08 23:26:26 -0400

    Pub upgrade

--- packages/curbo_mobile_utils/example/lib/buttons_pages.dart
+++ packages/curbo_mobile_utils/example/lib/buttons_pages.dart
@@ -2,7 +2,7 @@ import 'package:curbo_mobile_utils/curbo_mobile_utils.dart';
 import 'package:flutter/material.dart';
 
 class ButtonsPage extends StatelessWidget {
-  const ButtonsPage({Key key}) : super(key: key);
+  const ButtonsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
